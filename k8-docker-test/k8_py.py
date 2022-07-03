9468acfc74a46590dd6a07c641ef2ae4c80c715b
Syabz03 <2609746B@student.gla.ac.uk>
2022-07-03 22:54:59 +0800

    added (test) files for deploying .py to docker + k8

---
+++ k8-docker-test/k8_py.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import time
+import os
+
+i=0
+__file__ = "test_file.txt"
+while True:
+    # datetime object containing current date and time
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    # Writing to file
+    with open(__file__, "a+") as file1:
+        # Writing data to a file
+        file1.write("Hello \n")
+        file1.write(str(dt_string))
+        file1.write("\n")
+    
+    # Reading from file
+    with open(__file__, "r+") as file1:
+        # Reading form a file
+        print(file1.read())
+
+    print("Path at terminal when executing this file")
+    print(os.getcwd() + "\n")
+
+    print("This file full path (following symlinks)")
+    full_path = os.path.realpath(__file__)
+    print(full_path + "\n")
+
+    print("This file directory only")
+    print(os.path.dirname(full_path))
+
+    time.sleep(10)
