73b7230367267e23d751858ec860142fec7cc2a7
MinimanXC <minimanxc@gmail.com>
2022-07-04 20:31:59 +0800

    Updated blockchain to be permissioned

--- blockchain.py
+++ blockchain.py
@@ -9,18 +9,22 @@ GENESIS_HASH = '0' * 64 # Constant value of 64 zeros. Used by genesis block as b
 class Block:
 
     _blockId: int
+    _hash: str
+    _prevHash: str
     previous: Optional['Block']
     _data: Optional[dict] # Dictionary which includes timestamp, transaction data and wtv data that needs to be added
     _nonce: int
 
     # Old constructor (but might still be relevant depending on code structure)
-    def __init__(self, previous=None, data=None, nonce=0) -> None:
+    def __init__(self, previous=None, data=None, nonce=0, hash=None, prevHash=None) -> None:
 
         if previous is None:  # If genesis block, id will be 0
             self._blockId = 0
         else:  # Else block id will be previous block id + 1
             self._blockId = previous.getBlockID() + 1
 
+        self._hash = hash
+        self._prevHash = prevHash
         self._data = data
         self.previous = previous
         self._nonce = nonce
@@ -28,6 +32,8 @@ class Block:
     # Always instantiate using this constructor unless you know what you're doing
     def __init__(self, data=None) -> None:
 
+        self._hash = None
+        self._prevHash = None
         self._blockId = 0
         self._data = data
         self.previous = None
@@ -36,12 +42,24 @@ class Block:
     # Recursive function 
     def getPreviousHash(self) -> str:
         if self.previous is None: # Base case of recursive function. Genesis block has no previous hash thus default to constant value of 0
-            return GENESIS_HASH
+            self._prevHash = GENESIS_HASH
+            return self._prevHash
         else:
-            return self.previous.getHash()
+            self._prevHash = self.previous.getHash()
+            return self._prevHash
 
+    # Function to calculate hash based on previous hash and current data. Do not call if validity of block needs to be checked
     def getHash(self) -> str:
-        return calculateHash(self._blockId, self.getPreviousHash(), self._data, self._nonce)
+        self._hash = calculateHash(self._blockId, self.getPreviousHash(), self._data, self._nonce)
+        return self._hash
+
+    # Returns only the hash string instead of recalculating the hash and returning the updated hash
+    def getPreviousCalculatedHash(self) -> str:
+        return self._prevHash
+
+    # Returns only the previous hash string instead of recalculating the previous hash and returning the updated previous hash
+    def getCalculatedHash(self) -> str:
+        return self._hash
 
     # Function may be removed if adopting permissioned blockchain. Replace with cryptography keys for authentication of users(?)
     def mine(self, zeroCount) -> None:
@@ -55,6 +73,10 @@ class Block:
     def getBlockID(self) -> int:
         return self._blockId
 
+    def setData(self, data) -> dict:
+        self._data = data
+        self.getHash()
+
     def getData(self) -> dict:
         return self._data
 
@@ -64,8 +86,8 @@ class Block:
     def __str__(self) -> str:
         return str('\nBlock %s\nHash: %s\nPrevious Hash: %s\nData: %s\nNonce: %s' % (
             self._blockId,
-            self.getHash(),
-            self.getPreviousHash(),
+            self._hash,
+            self._prevHash,
             str(self._data),
             self._nonce)
         )
@@ -82,15 +104,17 @@ class Blockchain():
 
     def addBlock(self, block: Block) -> None:
         if self.chain.size() == 0:
-            self.mineBlock(block)
+            #self.mineBlock(block)
+            block.getHash()
             self.chain.insert(block)
         else:
             block.previous = self.chain.head.data
             block.setBlockID(block.previous.getBlockID() + 1)
-            self.mineBlock(block)
+            block.getHash()
+            #self.mineBlock(block)
             self.chain.insert(block)
 
-
+    # Do not call. Mining not needed in permissioned blockchain
     def mineBlock(self, block: Block) -> None:
         block.mine(self.difficulty) # Getting difficulty issues because im not updating the block id at this point in time
 
@@ -99,10 +123,14 @@ class Blockchain():
         chainList = self.chain.toArray()
 
         for i in range(0, len(chainList) - 1): # Iterate through each block, ignoring the genesis block (the last element in the array)
-            prevHashInBlock = chainList[i].getPreviousHash()
-            hashInPrevBlock = chainList[i + 1].getHash()
+            prevHashInBlock = chainList[i].getPreviousCalculatedHash()
+            hashInPrevBlock = chainList[i + 1].getCalculatedHash()
 
-            if prevHashInBlock != hashInPrevBlock or hashInPrevBlock[:self.difficulty] != '0' * self.difficulty:
+            # Uncomment only if mining is needed again
+            # if prevHashInBlock != hashInPrevBlock or hashInPrevBlock[:self.difficulty] != '0' * self.difficulty:
+            #     return False
+
+            if prevHashInBlock != hashInPrevBlock:
                 return False
 
         return True
@@ -144,7 +172,7 @@ if __name__ == '__main__':
 
     # Trying to invalidate the block to test validity function
     
-    blockchain.chain.head.prev.data._data = "Yo mama"
-    blockchain.chain.head.data.mine(4)
+    blockchain.chain.head.prev.data.setData("Yo mama")
+    #blockchain.chain.head.prev.data.mine(0)
     blockchain.printChain()
     print("The blockchain's validity is", blockchain.isValid())
